d86a15bf3b40919d0e28657e93adf094d8d0bcde
lacastrilf <lacastrilf@eafit.edu.co>
2025-07-31 10:40:24 -0500

    First Commit: Add project

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse("<h1>Welcome to the Movie Reviews Home Page!</h1>")
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Laura Castrillón'})
+
+def about(request):
+    return HttpResponse("<h1>About Movie Reviews</h1><p>This is a platform to share and read movie reviews.</p>")
