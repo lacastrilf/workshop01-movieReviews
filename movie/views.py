7ed179585ecad0dd9e51b0240e395766858867af
lacastrilf <lacastrilf@eafit.edu.co>
2025-08-01 21:07:55 -0500

    Movie search

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie 
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse("<h1>Welcome to the Movie Reviews Home Page!</h1>")
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Laura Andrea Castrillón Fajardo'})
+    #return render(request, 'home.html', {'name': 'Laura Andrea Castrillón Fajardo'})
+    #searchTerm = request.GET.get('searchMovie')
+    #movies = Movie.objects.all()
+    #return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
